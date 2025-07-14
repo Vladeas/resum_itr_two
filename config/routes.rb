47d7fb452353201c2774fa4301f6dcaa3b9962dd
Vlad Danciu <vlad.danciu@pm.me>
2025-07-14 20:01:57 +0300

    g Users controlle

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
+  get "users/update"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # get "home/index"
