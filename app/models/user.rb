9041a5998dc3d70a8f3b5203a8fa05cc4505709f
Vlad Danciu <vlad.danciu@pm.me>
2025-07-21 13:23:18 +0300

    Model update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :user_projects, foreign_key: "user_id", dependant: :destroy
+
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "date_of_birth", "description", "driving_license", "email", "encrypted_password", "forename", "github",
      "id", "id_value", "linkedin", "phone_number", "remember_created_at", "reset_password_sent_at", "reset_password_token",
