6972a31032ec99abc5a503ff1a8d4159a5fa4107
Vlad Danciu <vlad.danciu@pm.me>
2025-07-21 17:33:33 +0300

    UserProjects consistency name fix (:description,:content)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_21_135344) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_21_143008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_21_135344) do
   create_table "user_projects", force: :cascade do |t|
     t.string "title"
     t.string "link"
-    t.text "description"
+    t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
